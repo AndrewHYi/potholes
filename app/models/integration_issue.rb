041e0248d24591a3879496087a54c4d07bd28cbb
Andrew H Yi <andrew.yi50@gmail.com>
2022-07-13 04:25:29 -1000

    feat: relax integration_issue timestamp requirements

--- app/models/integration_issue.rb
+++ app/models/integration_issue.rb
@@ -12,7 +12,7 @@ class IntegrationIssue < ApplicationRecord
   # TODO: add columns or methods as needed
   # We are interested in saving the following information for each issue:
   # 1. ID of the remote issue
-  # 2. timestamps: created_at, acknowledged_at, closed_at, reopened_at, updated_at of the remote issue
+  # 2. timestamps: created_at, updated_at of the remote issue
   # 3. status
   # 4. summary
   # 5. coordinates
